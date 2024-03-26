8515ae776c6c7c0d2ca932fdbc2e441e0c5af226
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2024-03-26 19:46:29 +0100

    Fixed condition.

--- static/tests/backend/specs/exportHTML.js
+++ static/tests/backend/specs/exportHTML.js
@@ -2,32 +2,34 @@
 
 const common = require('ep_etherpad-lite/tests/backend/common');
 const randomString = require('ep_etherpad-lite/static/js/pad_utils').randomString;
+import {generateJWTToken, generateJWTTokenUser} from "ep_etherpad-lite/tests/backend/common";
 
 let agent;
-const apiKey = common.apiKey;
 const apiVersion = 1;
 
 const buildHTML = (body) => `<html><body>${body}</body></html>`;
 const getHTMLEndPointFor =
-    (padID, callback) => `/api/${apiVersion}/getHTML?apikey=${apiKey}&padID=${padID}`;
+    (padID, callback) => `/api/${apiVersion}/getHTML?padID=${padID}`;
 
 // Creates a pad and returns the pad id. Calls the callback when finished.
-const createPad = (padID, callback) => {
-  agent.get(`/api/${apiVersion}/createPad?apikey=${apiKey}&padID=${padID}`)
-      .end((err, res) => {
-        if (err || (res.body.code !== 0)) callback(new Error('Unable to create new Pad'));
+const createPad = async (padID, callback) => {
+    agent.get(`/api/${apiVersion}/createPad?padID=${padID}`)
+        .set("Authorization", await generateJWTToken())
+        .end((err, res) => {
+            if (err || (res.body.code !== 0)) callback(new Error('Unable to create new Pad'));
 
-        callback(padID);
-      });
+            callback(padID);
+        });
 };
 
-const setHTML = (padID, html, callback) => {
-  agent.get(`/api/${apiVersion}/setHTML?apikey=${apiKey}&padID=${padID}&html=${html}`)
-      .end((err, res) => {
-        if (err || (res.body.code !== 0)) callback(new Error('Unable to set pad HTML'));
+const setHTML = async (padID, html, callback) => {
+    agent.get(`/api/${apiVersion}/setHTML?padID=${padID}&html=${html}`)
+        .set("Authorization", await generateJWTToken())
+        .end((err, res) => {
+            if (err || (res.body.code !== 0)) callback(new Error('Unable to set pad HTML'));
 
-        callback(null, padID);
-      });
+            callback(null, padID);
+        });
 };
 
 describe('export Subscript to HTML', function () {
@@ -37,12 +39,12 @@ describe('export Subscript to HTML', function () {
   before(async function () { agent = await common.init(); });
 
   // create a new pad before each test run
-  beforeEach(function (done) {
-    padID = randomString(5);
+  beforeEach(async function (done) {
+      padID = randomString(5);
 
-    createPad(padID, () => {
-      setHTML(padID, html(), done);
-    });
+      await createPad(padID, async () => {
+          await setHTML(padID, html(), done);
+      });
   });
 
   context('when pad text has one Subscript', function () {
@@ -50,21 +52,23 @@ describe('export Subscript to HTML', function () {
       html = () => buildHTML('<sub>Hello world</sub>');
     });
 
-    it('returns ok', function (done) {
-      agent.get(getHTMLEndPointFor(padID))
-          .expect('Content-Type', /json/)
-          .expect(200, done);
+    it('returns ok', async function (done) {
+        agent.get(getHTMLEndPointFor(padID))
+            .set("Authorization", await generateJWTToken())
+            .expect('Content-Type', /json/)
+            .expect(200, done);
     });
 
-    it('returns HTML with Subscript HTML tags', function (done) {
-      agent.get(getHTMLEndPointFor(padID))
-          .expect((res) => {
-            const html = res.body.data.html;
-            if (html.indexOf('<sub>Hello world</sub>') === -1) {
-              throw new Error('No sub tag detected');
-            }
-          })
-          .end(done);
+    it('returns HTML with Subscript HTML tags', async function (done) {
+        agent.get(getHTMLEndPointFor(padID))
+            .set("Authorization", await generateJWTToken())
+            .expect((res) => {
+                const html = res.body.data.html;
+                if (html.indexOf('<sub>Hello world</sub>') === -1) {
+                    throw new Error('No sub tag detected');
+                }
+            })
+            .end(done);
     });
   });
 
@@ -74,22 +78,24 @@ describe('export Subscript to HTML', function () {
     });
 
 
-    it('returns ok', function (done) {
-      agent.get(getHTMLEndPointFor(padID))
-          .expect('Content-Type', /json/)
-          .expect(200, done);
+    it('returns ok', async function (done) {
+        agent.get(getHTMLEndPointFor(padID))
+            .set("Authorization", await generateJWTToken())
+            .expect('Content-Type', /json/)
+            .expect(200, done);
     });
 
-    it('returns HTML with Multiple Subscripts HTML tags', function (done) {
-      agent.get(getHTMLEndPointFor(padID))
-          .expect((res) => {
-            const html = res.body.data.html;
-            if (html.indexOf('<sub>Hello world</sub>') === -1) {
-              throw new Error('No sub tag detected');
-            }
-            if (html.indexOf('<sub>Foo</sub>') === -1) throw new Error('No sub tag detected');
-          })
-          .end(done);
+    it('returns HTML with Multiple Subscripts HTML tags', async function (done) {
+        agent.get(getHTMLEndPointFor(padID))
+            .set("Authorization", await generateJWTToken())
+            .expect((res) => {
+                const html = res.body.data.html;
+                if (html.indexOf('<sub>Hello world</sub>') === -1) {
+                    throw new Error('No sub tag detected');
+                }
+                if (html.indexOf('<sub>Foo</sub>') === -1) throw new Error('No sub tag detected');
+            })
+            .end(done);
     });
   });
 });
@@ -113,21 +119,23 @@ describe('export Superscript to HTML', function () {
     });
 
 
-    it('returns ok', function (done) {
-      agent.get(getHTMLEndPointFor(padID))
-          .expect('Content-Type', /json/)
-          .expect(200, done);
+    it('returns ok', async function (done) {
+        agent.get(getHTMLEndPointFor(padID))
+            .set("Authorization", await generateJWTToken())
+            .expect('Content-Type', /json/)
+            .expect(200, done);
     });
 
-    it('returns HTML with Suberscript HTML tags', function (done) {
-      agent.get(getHTMLEndPointFor(padID))
-          .expect((res) => {
-            const html = res.body.data.html;
-            if (html.indexOf('<sup>Hello world</sup>') === -1) {
-              throw new Error('No sup tag detected');
-            }
-          })
-          .end(done);
+    it('returns HTML with Suberscript HTML tags', async function (done) {
+        agent.get(getHTMLEndPointFor(padID))
+            .set("Authorization", await generateJWTToken())
+            .expect((res) => {
+                const html = res.body.data.html;
+                if (html.indexOf('<sup>Hello world</sup>') === -1) {
+                    throw new Error('No sup tag detected');
+                }
+            })
+            .end(done);
     });
   });
 
@@ -136,24 +144,26 @@ describe('export Superscript to HTML', function () {
       html = () => buildHTML('<sup>Hello world</sup><br/><sup>Foo</sup>');
     });
 
-    it('returns ok', function (done) {
-      agent.get(getHTMLEndPointFor(padID))
-          .expect('Content-Type', /json/)
-          .expect(200, done);
+    it('returns ok', async function (done) {
+        agent.get(getHTMLEndPointFor(padID))
+            .set("Authorization", await generateJWTToken())
+            .expect('Content-Type', /json/)
+            .expect(200, done);
     });
 
-    it('returns HTML with Multiple Superscripts HTML tags', function (done) {
-      agent.get(getHTMLEndPointFor(padID))
-          .expect((res) => {
-            const html = res.body.data.html;
-            if (html.indexOf('<sup>Hello world</sup>') === -1) {
-              throw new Error('No sup tag detected');
-            }
-            if (html.indexOf('<sup>Foo</sup>') === -1) {
-              throw new Error('No sup tag detected');
-            }
-          })
-          .end(done);
+    it('returns HTML with Multiple Superscripts HTML tags', async function (done) {
+        agent.get(getHTMLEndPointFor(padID))
+            .set("Authorization", await generateJWTToken())
+            .expect((res) => {
+                const html = res.body.data.html;
+                if (html.indexOf('<sup>Hello world</sup>') === -1) {
+                    throw new Error('No sup tag detected');
+                }
+                if (html.indexOf('<sup>Foo</sup>') === -1) {
+                    throw new Error('No sup tag detected');
+                }
+            })
+            .end(done);
     });
   });
 });
